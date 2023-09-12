d0d41b90cdb973c87bc40db4340cb934d73cfd61
Inviber <bkelley4600@gmail.com>
2023-09-11 21:20:54 -0500

    count characters func

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def count_letters(str):
+    characters = {}
+    count = 0
+    for i in str:
+        i = i.lower()
+        if i in characters.keys():
+            characters[i] += 1
+            continue
+        else:
+            characters.update({i: count+1})
+    print(characters)
+
 def count_words(str):
     words = str.split()
     print(len(words))
@@ -7,6 +19,8 @@ def read_book(book):
         content = f.read()
         print(content)
         count_words(content)
+        count_letters(content)
+
 
 def main():
     read_book("books/frankenstein.txt")
