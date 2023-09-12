6fb1c373e66f349f1c80ed67e7c7615456b39c5b
Inviber <bkelley4600@gmail.com>
2023-09-11 21:01:11 -0500

    read book func, word count func

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+def count_words(str):
+    words = str.split()
+    print(len(words))
+
+def read_book(book):
+    with open(book) as f:
         content = f.read()
         print(content)
+        count_words(content)
+
+def main():
+    read_book("books/frankenstein.txt")
 
 main()
