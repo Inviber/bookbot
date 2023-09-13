7b3af562bf4fcd6d278ea59927d1cafc2f1f2efa
Inviber <bkelley4600@gmail.com>
2023-09-12 20:22:42 -0500

    Finished bookbot

--- main.py
+++ main.py
@@ -7,20 +7,29 @@ def count_letters(str):
             characters[i] += 1
             continue
         else:
-            characters.update({i: count+1})
-    print(characters)
+            if i.isalpha():
+                characters.update({i: count+1})
+    return characters
 
 def count_words(str):
     words = str.split()
-    print(len(words))
+    return len(words)
+
+def book_report(book, words, characters):
+    print(f"--- Begin report of {book} ---")
+    print(f"{words} words in the document\n")
+    report = dict(reversed(sorted(characters.items(), key=lambda item: item[1])))
+    for i in report:
+        print(f"The '{i}' was found {report[i]} times")
+    print("--- End Report ---")
 
 def read_book(book):
     with open(book) as f:
         content = f.read()
         print(content)
-        count_words(content)
-        count_letters(content)
-
+        print(count_words(content))
+        print(count_letters(content))
+        book_report(book, count_words(content), count_letters(content))
 
 def main():
     read_book("books/frankenstein.txt")
